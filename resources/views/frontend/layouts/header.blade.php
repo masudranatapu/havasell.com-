9ab9baa0b65c1c811183e2ee26522bfcc91d76b8
RABIN <rabinmia7578@gmail.com>
2023-02-26 16:24:46 +0600

    update

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -4,10 +4,22 @@
             <a class="navbar-brand" href="{{ route('frontend.index') }}">
                 <img src="{{ asset('frontend/images/logo.png') }}" width="124" alt="logo">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <ul class="navbar-nav d-block d-lg-none">
+                <li class="nav-item">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </li>
+                <li class="nav-item custom-list-style">
+                    <a href="{{ route('frontend.post.create') }}">
+                        <i class="la la-plus d-block d-sm-none"></i>
+                        {{ __('Create a posting') }}
+                    </a>
+                </li>
+            </ul>
+
+
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
@@ -20,7 +32,7 @@
                         <a class="nav-link" href="{{ route('frontend.search') }}">{{ __('Ads') }}</a>
                     </li>
                 </ul>
-                <ul class="navbar-nav ms-lg-auto align-items-lg-center mt-3">
+                <ul class="navbar-nav ms-lg-auto align-items-lg-center mt-1">
                     @if (auth('user')->check())
                     <li class="nav-item">
                         <a class="nav-link " href="{{ route('user.profile') }}">{{ __('My Account') }}</a>
@@ -30,14 +42,14 @@
                         <a class="nav-link" href="{{ route('signin') }}">{{ __('My Account') }}</a>
                     </li>
                     @endif
-                    <li class="nav-item custom-list-style me-3">
+                    <li class="nav-item custom-list-style me-3 d-none d-lg-block">
                         <a href="{{ route('frontend.post.create') }}">{{ __('Create a
                             posting') }}</a>
                     </li>
                     <div class="btn-group language_dropdown">
                         <form action="{{ route('frontend.localization') }}" method="post">
                             @csrf
-                            <select name="language" id="language" class="form-control form-select mb-3"
+                            <select name="language" id="language" class="form-control form-select"
                                 onchange="this.form.submit()">
                                 <option value="en" @if (Session::get('locale')=='en' ) selected @endif>
                                     English
